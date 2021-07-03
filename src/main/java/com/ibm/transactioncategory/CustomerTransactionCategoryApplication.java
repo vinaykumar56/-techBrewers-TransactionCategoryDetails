d6c53e1dad9faca13617c78dcc4f95fd5b1d7c04
unknown <vinay7724@gmail.com>
2021-07-04 00:52:12 +0530

    initial changes

---
+++ src/main/java/com/ibm/transactioncategory/CustomerTransactionCategoryApplication.java
@@ -0,0 +1,17 @@
+package com.ibm.transactioncategory;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CustomerTransactionCategoryApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTransactionCategoryApplication.class, args);
+		
+	}
+
+}
